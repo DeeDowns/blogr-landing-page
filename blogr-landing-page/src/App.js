c9d1b9dcab55f748e193e86a443e043d2e30cbbe
DeeDowns <dee_downs@outlook.com>
2021-03-29 13:24:15 -0400

    install react

---
+++ blogr-landing-page/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      </header>
+      <div class="attribution">
+        Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" rel="noreferrer">Frontend Mentor</a>. 
+        Coded by <a href="#">Dee</a>.
+      </div>
+    </div>
+  );
+}
+
+export default App;
