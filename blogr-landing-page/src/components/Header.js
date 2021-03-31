0d0a4c9150c7a145221e26f8fe88befd191496df
DeeDowns <dee_downs@outlook.com>
2021-03-31 16:58:27 -0400

    restructure header

--- blogr-landing-page/src/components/Header.js
+++ blogr-landing-page/src/components/Header.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import blogrlogo from '../images/logo.svg'
+import iconArrow from '../images/icon-arrow-light.svg'
 import './Header.css';
 
 const Header = () => {
@@ -7,40 +8,40 @@ const Header = () => {
 
     return (
         <header>
-            <div className='intro-div'>
-                <img src={blogrlogo} alt='blogr logo' />
+            <div className='intro-div'> 
                 <nav>
-                    <label>Product</label>
-                    <select>
-                    <option>Overview</option>
-                    <option>Pricing</option>
-                    <option>Marketplace</option>
-                    <option>Features</option>
-                    <option>Integrations</option>
-                    </select>
-                    
-                    <label>Company</label>
-                    <select>
-                    <option>About</option>
-                    <option>Team</option>
-                    <option>Blog</option>
-                    <option>Careers</option>
-                    </select>
+                    <img src={blogrlogo} alt='blogr logo' />
+                    <div className='dropdowns'>
+                        <ul>Product <img src={iconArrow} alt='arrow' />
+                            <li><a href='#'>Overview</a></li>
+                            <li><a href='#'>Pricing</a></li>
+                            <li><a href='#'>Marketplace</a></li>
+                            <li><a href='#'>Features</a></li>
+                            <li><a href='#'>Integrations</a></li>
+                        </ul>
 
-                    <label>Connect</label>
-                    <select>
-                    <option>Newsletter</option>
-                    <option>LinkedIn</option>
-                    </select>
-                    <div>
-                        <p>Login</p>
-                        <p>Sign Up</p>
+                        <ul>Company <img src={iconArrow} alt='arrow' />
+                            <li><a href='#'>About</a></li>
+                            <li><a href='#'>Team</a></li>
+                            <li><a href='#'>Blog</a></li>
+                            <li><a href='#'>Careers</a></li>
+                        </ul>
+
+                        <ul>Connect <img src={iconArrow} alt='arrow' />
+                            <li><a href='#'>Newsletter</a></li>
+                            <li><a href='#'>LinkedIn</a></li>
+                        </ul>
                     </div>
+
+                    <div className='login-signup'>
+                            <a href='#'>Login</a>
+                            <a href='#'>Sign Up</a>
+                        </div>
                 </nav>
-                <div>
+                <div className='description'> 
                     <h1>A modern publishing platform</h1>
                     <h2>Grow your audience and build your online brand</h2>
-                    <div>
+                    <div className='button-group'>
                         <button>Start for Free</button>
                         <button>Learn More</button>
                     </div>
