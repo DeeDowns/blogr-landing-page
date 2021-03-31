1868ea2b4858c1e2d90b431fe93e2e7738b21f55
DeeDowns <dee_downs@outlook.com>
2021-03-31 14:14:51 -0400

    position svgs and contents of main section

--- blogr-landing-page/src/components/Main.js
+++ blogr-landing-page/src/components/Main.js
@@ -16,10 +16,12 @@ const Main = () => {
   The editor supports management of multiple blogs and allows easy manipulation of embeds such as images, 
   videos, and Markdown. Extensibility with plugins and themes provide easy ways to add functionality or 
   change the looks of a blog.</p>
-                </div>
-                <h4>Robust content management</h4>
-                <p>  Flexible content management enables users to easily move through posts. Increase the usability of your blog 
+                
+                    <h4>Robust content management</h4>
+                    <p>  Flexible content management enables users to easily move through posts. Increase the usability of your blog 
   by adding customized categories, sections, format, or flow. With this functionality, you’re in full control.</p>
+                </div>
+
                 <div className='section-one-img'>
                     <img src={editorSvgDesktop} alt='editor' />
                 </div>
@@ -29,22 +31,28 @@ const Main = () => {
                 <div className='section-one-img'>
                     <img src={phonesSvg} alt='editor' />
                 </div>
-                <h4>State of the Art Infrastructure</h4>
-                <p>  With reliability and speed in mind, worldwide data centers provide the backbone for ultra-fast connectivity. 
+
+                <div className='section-two-content'>
+                    <h4>State of the Art Infrastructure</h4>
+                    <p>  With reliability and speed in mind, worldwide data centers provide the backbone for ultra-fast connectivity. 
   This ensures your site will load instantly, no matter where your readers are, keeping your site competitive.</p>
+                </div>
             </section>
 
             <section className='section-three'>
                 <div className='section-one-img'>
                     <img src={laptopSvgDesktop} alt='editor' />
                 </div>
-                <h4>Free, open, simple</h4>
-                <p>Blogr is a free and open source application backed by a large community of helpful developers. It supports 
-  features such as code syntax highlighting, RSS feeds, social media integration, third-party commenting tools, 
-  and works seamlessly with Google Analytics. The architecture is clean and is relatively easy to learn.</p>
-                <h4>Powerful tooling</h4>
-                <p>Batteries included. We built a simple and straightforward CLI tool that makes customization and deployment a breeze, but
+                
+                <div className='section-two-content'>
+                    <h4>Free, open, simple</h4>
+                    <p>Blogr is a free and open source application backed by a large community of helpful developers. It supports 
+    features such as code syntax highlighting, RSS feeds, social media integration, third-party commenting tools, 
+    and works seamlessly with Google Analytics. The architecture is clean and is relatively easy to learn.</p>
+                    <h4>Powerful tooling</h4>
+                    <p>Batteries included. We built a simple and straightforward CLI tool that makes customization and deployment a breeze, but
   capable of producing even the most complicated sites.</p>
+                </div>
             </section>
         </main>
     )
