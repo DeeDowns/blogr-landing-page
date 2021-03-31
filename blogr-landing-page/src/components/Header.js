6b00c80223605c83dced6324a049fece1f0fd803
DeeDowns <dee_downs@outlook.com>
2021-03-31 13:51:53 -0400

    clean up header structure and some styling

--- blogr-landing-page/src/components/Header.js
+++ blogr-landing-page/src/components/Header.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import blogrlogo from '../images/logo.svg'
-import bgPatternIntro from '../images/bg-pattern-intro.svg'
 import './Header.css';
 
 const Header = () => {
@@ -9,43 +8,43 @@ const Header = () => {
     return (
         <header>
             <div className='intro-div'>
-            <img src={blogrlogo} alt='blogr logo' />
-            <nav>
-                <label>Product</label>
-                <select>
-                <option>Overview</option>
-                <option>Pricing</option>
-                <option>Marketplace</option>
-                <option>Features</option>
-                <option>Integrations</option>
-                </select>
-                
-                <label>Company</label>
-                <select>
-                <option>About</option>
-                <option>Team</option>
-                <option>Blog</option>
-                <option>Careers</option>
-                </select>
+                <img src={blogrlogo} alt='blogr logo' />
+                <nav>
+                    <label>Product</label>
+                    <select>
+                    <option>Overview</option>
+                    <option>Pricing</option>
+                    <option>Marketplace</option>
+                    <option>Features</option>
+                    <option>Integrations</option>
+                    </select>
+                    
+                    <label>Company</label>
+                    <select>
+                    <option>About</option>
+                    <option>Team</option>
+                    <option>Blog</option>
+                    <option>Careers</option>
+                    </select>
 
-                <label>Connect</label>
-                <select>
-                <option>Newsletter</option>
-                <option>LinkedIn</option>
-                </select>
+                    <label>Connect</label>
+                    <select>
+                    <option>Newsletter</option>
+                    <option>LinkedIn</option>
+                    </select>
+                    <div>
+                        <p>Login</p>
+                        <p>Sign Up</p>
+                    </div>
+                </nav>
                 <div>
-                    <p>Login</p>
-                    <p>Sign Up</p>
+                    <h1>A modern publishing platform</h1>
+                    <h2>Grow your audience and build your online brand</h2>
+                    <div>
+                        <button>Start for Free</button>
+                        <button>Learn More</button>
+                    </div>
                 </div>
-            </nav>
-            <div>
-                <h1>A modern publishing platform</h1>
-                <h2>Grow your audience and build your online brand</h2>
-                <div>
-                    <button>Start for Free</button>
-                    <button>Learn More</button>
-                </div>
-            </div>
             </div>
         </header>
     )
