a2fded8ab98c98d307e1b27269facbbc90cb4d1b
DeeDowns <dee_downs@outlook.com>
2021-03-31 13:18:09 -0400

    create Main component and set up rough structure

--- blogr-landing-page/src/App.js
+++ blogr-landing-page/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Header from './components/Header'
+import Main from './components/Main'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-     <Header/>
+     <Header />
+     <Main />
       <div>
         Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" rel="noreferrer">Frontend Mentor</a>. 
         Coded by <a href="https://github.com/DeeDowns" target='_blank' rel="noreferrer" >Dee</a>.
